5bf3ce80500416a5d40a449cc48e72fe6c22dbb3
carlosot <c.texeirajr@gmail.com>
2018-09-15 18:52:50 -0300

    learning JS

---
+++ consultas/arquivos-js/palindrome.js
@@ -0,0 +1,12 @@
+//2) Receba uma palavra e determine se ela é um palindrome.
+
+function Palindrome(palavra){
+  var splitString = palavra.split("");
+  var reverseArray = splitString.reverse();
+  var joinArray = reverseArray.join("");
+  if (joinArray === palavra){
+    return alert("É palidrome")
+  }else{
+    return alert("Não é palidrome")
+  }
+}
---
+++ consultas/arquivos-js/retornaMaior.js
@@ -0,0 +1,12 @@
+//1) Receba dois números e retorne qual deles é o maior.
+
+function retornaMaior(a, b){
+  if(a > b){
+    return alert(a)
+  } else if(b > a){
+    return alert(b)
+  }else{
+    return alert('Iguais')
+  }
+}
+
