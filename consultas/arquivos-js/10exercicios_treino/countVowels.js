e1160f50548bd66b78d39dbf8b68a342f938149a
Carlos Teixeira <39592947+CarlosOT@users.noreply.github.com>
2018-09-21 22:19:53 -0300

    Update countVowels.js

--- consultas/arquivos-js/10exercicios_treino/countVowels.js
+++ consultas/arquivos-js/10exercicios_treino/countVowels.js
@@ -1,14 +1,20 @@
 //9) Construa uma função que recebe uma palavra e devolve quantas vogais existe nesta palavra.
 
-function countVowels(palavra){
-  var vowels = ["a","A","e","E","i","I","o","O","u","U"];
-  var counter = 0;
-  for(i = 0; i < palavra.length; i++){
-    var char = palavra[i];
-    if (vowels.includes(char)){
-      counter += 1;
-    }
+<html lang="pt">
+
+<head>
+    <meta charset="utf-8" />
+  <script>
+
+  function countVowels(){
+    var palavra = document.getElementById('palavra').value;
+    var vowels = ["a","A","e","E","i","I","o","O","u","U"];
+    var counter = 0;
+    for(i = 0; i < palavra.length; i++){
+    if (vowels.includes(palavra[i])){
+    counter += 1;
   }
+}
   if (counter > 1){
     return alert(`Essa palavra tem ${counter} vogais `);
   }else if (counter == 1){
@@ -17,3 +23,24 @@ function countVowels(palavra){
     return alert(`Essa palavra não tem vogal`);
   }
 }
+
+  </script>
+
+</head>
+
+  <body>
+
+   <form onsubmit="javascript: return countVowels()">
+
+        <label for="palavra">Palavra</label> <br>
+        <input type="text" id="palavra"> <br>
+
+        <input type="submit" value="Enviar">
+
+   </form>
+
+
+  </body>
+
+</html>
+
