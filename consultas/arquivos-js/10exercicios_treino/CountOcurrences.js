658315417444761db7b74a2d2f2eee8dc63f7489
Carlos Teixeira <39592947+CarlosOT@users.noreply.github.com>
2018-09-22 21:07:25 -0300

    Delete CountOcurrences.js

--- consultas/arquivos-js/10exercicios_treino/CountOcurrences.js
+++
@@ -1,9 +0,0 @@
-//8) Construa uma função que verifica quantos A's  (caixa alta e baixa) existem em uma palavra.
-
-function countOcurrences(string){
-  var counter1 = (string.match(/A/g) || []).length;
-  var counter2 = (string.match(/a/g) || []).length;
-  stringView = `Essa cadeia de caracteres tem ${counter1} letras "A" e ${counter2} letras "a".`
-  return alert(stringView);
-}
-
