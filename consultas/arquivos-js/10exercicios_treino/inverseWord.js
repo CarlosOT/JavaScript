bb118ce2587a966af8348b0e88c99970389c7ca8
Carlos Teixeira <39592947+CarlosOT@users.noreply.github.com>
2018-09-23 15:50:26 -0300

    Delete inverseWord.js

--- consultas/arquivos-js/10exercicios_treino/inverseWord.js
+++
@@ -1,6 +0,0 @@
-//10) Construa uma função que inverta uma palavra e retorne esta palavra invertida.
-
-function inverseWord(){
-  var inverse = document.getElementById('palavra').value;
-  return alert(inverse.split("").reverse().join(""));
-}
